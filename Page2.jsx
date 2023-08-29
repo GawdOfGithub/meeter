b527d586ddaf8631c4ec6e2b3d8d2bc405ee9d41
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-08-29 08:23:59 +0530

    title changed

--- Page2.jsx
+++ Page2.jsx
@@ -4,18 +4,18 @@ import TeamMemberCard from './teamMemberCard';
 import member from './teamData';
 
 const Page2 = () => {
-  const cardsPerRow = { base: 2, md: 6 };
-  const cardWidth = { base: '100%', md: '20%', lg: '33.33%' };
+  const cardsPerRow = { base: 2, md: 3, lg: 4, xl: 5 }; // Adjust the numbers as needed
+  const cardWidth = { base: '100%', md: '50%', lg: '33.33%', xl: '25%' };
 
   return (
     <Container maxW="200vh">
       <Center>
-        <TeamMemberCard member={member[0]} style={{ width: cardWidth }} />
+        <TeamMemberCard member={member[0]} style={{ width: cardWidth.base }} />
       </Center>
 
-      <SimpleGrid columns={cardsPerRow} spacing={4} mt="4">
-        {member.slice(1, 13).map((member) => (
-          <TeamMemberCard key={member.id} member={member} style={{ width: cardWidth }} />
+      <SimpleGrid columns={4} spacing={4} mt="4">
+        {member.slice(1, 30).map((member) => (
+          <TeamMemberCard key={member.id} member={member} style={{ width: cardWidth.base }} />
         ))}
       </SimpleGrid>
     </Container>
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useRef } from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import p2 from './p2.jpg';
-import Page1 from '../page1';
-// import Page2 from '../page2';
-// import Page3 from '../page3';
+import p4 from './p4.jpg';
+import Page1 from '../Page1';
+import Page2 from '../Page2';
+// import Page3 from '../Page3';
 const url = (name, wrap = false) =>
   `${wrap ? 'url(' : ''}${name}${wrap ? ')' : ''}`;
 
@@ -19,26 +19,26 @@ function App() {
           speed={0}
           factor={3}
           style={{
-            backgroundImage: url(p2, true),
+            backgroundImage: url(p4, true),
             backgroundSize: 'cover',
             zIndex: -1,
           }}
         />
 
         {/* Page 1 */}
-        <ParallaxLayer offset={0} speed={1} style={{ height: '100vh' }}>
+        <ParallaxLayer offset={0} speed={1.5} style={{ height: '100vh' }}>
           <Page1 />
         </ParallaxLayer>
 
         { /* Page 2 */}
-        {/* <ParallaxLayer offset={1.5} speed={1} style={{ height: '200vh' }}>
+        <ParallaxLayer offset={1} speed={1.5} style={{ height: '100vh' }}>
           <Page2 />
-        </ParallaxLayer> */}
+        </ParallaxLayer>
 
-        {/* Duplicate Page 2 */}
-        {/* <ParallaxLayer offset={4} speed={1} style={{ height: '400vh' }}>
+    
+         <ParallaxLayer offset={5} speed={1} style={{ height: '200vh' }}>
           <Page2 />
-        </ParallaxLayer> */}
+        </ParallaxLayer> 
 
         {/* Page 3 */}
         {/* <ParallaxLayer offset={5} speed={1} style={{ height: '500vh' }}>
